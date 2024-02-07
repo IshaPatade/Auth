36dd2a022afed4b241533dd45323f6d42992c151
IshaPatade <ishaapatade14@gmail.com>
2024-02-07 22:19:27 +0530

    some auth

---
+++ backend/models/userModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.statics.signup = async function (email, password, username) {
+  const exists = await this.findOne({ email });
+  if (exists) {
+    throw error("Email already in use");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash, username });
+
+  return user;
+};
+
+module.exports = mongoose.model("User", userSchema);
